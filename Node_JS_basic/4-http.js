6818b57bd85b0c1b3e6c0efaa56cfa7e320c1382
SilviMarku <marku.silvia777@gmail.com>
2024-02-08 11:44:34 +0100

    Task

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,10 +1,12 @@
-const http = require('http');
+const http = require('node:http');
 
-const port = 1245;
-const app = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello Holberton School!');
-});
+const app = http
+  .createServer((req, response) => {
+    response.writeHead(200, {
+      'Content-Type': 'text/plain',
+    });
+    response.end('Hello Holberton School!');
+  })
+  .listen(1245);
 
-app.listen(port);
 module.exports = app;

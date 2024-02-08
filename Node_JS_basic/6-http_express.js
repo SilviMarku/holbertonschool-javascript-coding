00a91fd41e2a896c173eee6b75edbb0a7fae7a8a
SilviMarku <marku.silvia777@gmail.com>
2024-02-08 11:51:42 +0100

    Task

--- Node_JS_basic/6-http_express.js
+++ Node_JS_basic/6-http_express.js
@@ -3,7 +3,12 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello Holberton School!');
-}).listen(1245);
+  res.status(200).send('Hello Holberton School!');
+});
+app.listen(1245, (err) => {
+  if (err) {
+    console.log(err);
+  }
+});
 
 module.exports = app;
